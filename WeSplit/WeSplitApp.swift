048e71751c04e8ff8897b44d1d4752db6ddf6a15
Muhammad Naveed <muhammad.naveed@alliedc.com>
2024-08-05 18:37:11 +0500

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Naveed on 05/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
