f572f29199af9beedef7f940feddac2f227a251a
Muhammad Naveed <muhammad.naveed@alliedc.com>
2024-08-06 21:41:28 +0500

    add day 17

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,49 +8,61 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var email = ""
-    @State private var password = ""
-    @State private var showError = ""
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    @FocusState private var amountIsFocused: Bool
     
-    let userTypes = ["Admin", "Guest User"]
-    @State private var userType = ""
+    var totalPerPerson: Double {
+        let tipValue = checkAmount / 100 * Double(tipPercentage)
+        let grandTotal = checkAmount + tipValue
+        return grandTotal / Double(numberOfPeople)
+    }
+    
+    var totalAmountWithTip: Double {
+        let tipValue = checkAmount / 100 * Double(tipPercentage)
+        return checkAmount + tipValue
+    }
     
     var body: some View {
         NavigationStack{
             Form{
+                TextField("Amount",value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                    .keyboardType(.decimalPad)
+                    .focused($amountIsFocused)
+                
+                Picker("Number of people", selection: $numberOfPeople) {
+                    ForEach(2..<100, id: \.self) { people in
+                            Text("\(people) people")
+                        }
+                }.pickerStyle(.navigationLink)
                 
                 Section {
-                    Text("Email")
-                    TextField("enter email here!", text: $email)
-                    Text("Password")
-                    TextField("enter password here!", text: $password)
-                    
-                    if(!showError.isEmpty){
-                        Text(showError)
-                    }
-                }
-                
-                Section {
-                    Text("Select User Type")
-                    Picker("User Type", selection: $userType){
-                        ForEach(userTypes, id: \.self){ userType in
-                            Text(userType)
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(0..<101, id: \.self) {
+                            Text($0, format: .percent)
                         }
                     }
+                    .pickerStyle(.navigationLink)
                 }
                 
-                Button("Login") {
-                    if(email.isEmpty){
-                        showError = "Pleae Enter Email"
-                    } else {
-                        showError = ""
-                    }
-                
+                Section("Total amount with tip") {
+                    Text(totalAmountWithTip, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
                 }
-
-            }
-            .navigationTitle("User Login")
         
+                Section("Amount per person") {
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
+                
+            }
+            .navigationTitle("We Split")
+            .toolbar{
+                if amountIsFocused {
+                    Button("Done"){
+                        amountIsFocused = false
+                    }
+                }
+            }
         }
     }
 }
